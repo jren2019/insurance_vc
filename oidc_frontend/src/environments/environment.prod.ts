05b21fa2fbbde7ac7598e26f1182ba4cb5d8d74e
Jun Ren <jun.ren@facteon.global>
2025-08-20 00:54:04 +1200

    TBC

--- oidc_frontend/src/environments/environment.prod.ts
+++ oidc_frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
 	production: true,
-	apiUrl: 'http://issuance-vc.australiaeast.cloudapp.azure.com' // Calls will go to {apiUrl}/api/*
+	apiUrl: 'https://issuance-vc.australiaeast.cloudapp.azure.com' // Calls will go to {apiUrl}/api/*
 };
